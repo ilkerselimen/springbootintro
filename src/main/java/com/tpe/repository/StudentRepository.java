a3cb87ca21476ecc2ceee30ac632c9dda2b7fe28
İlker Şelimen <ilkerselimen@gmail.com>
2023-04-28 23:33:23 +0300

    repo

--- src/main/java/com/tpe/repository/StudentRepository.java
+++ src/main/java/com/tpe/repository/StudentRepository.java
@@ -4,9 +4,13 @@ import com.tpe.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository //Bu annotation'ı yazmasak da extends JpaRepository'den dolayı çalışır. Kod okunabilirliğini arttırmak için yine de yazıyoruz.
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-
     boolean existsByEmail(String email);
+
+    List<Student> findByLastName(String lastName);
+
 }
